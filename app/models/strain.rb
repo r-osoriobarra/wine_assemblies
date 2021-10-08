3c559c643f1c76e963a9558d03c0fc9affe9faa5
rodrigo <rob.developer@protonmail.com>
2021-10-08 01:52:59 -0300

    add strain model testing with rspec

--- app/models/strain.rb
+++ app/models/strain.rb
@@ -3,6 +3,11 @@ class Strain < ApplicationRecord
     has_many :blends
     has_many :wines, through: :blends, dependent: :destroy
 
+    #validations
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    
+
     def to_s
         self.name
     end
